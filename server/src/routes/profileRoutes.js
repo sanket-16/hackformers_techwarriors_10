e8cc21936c08bc75c3e97431c961e8123805170e
Bhushan <bhushanpawar9001@gmail.com>
2023-03-19 02:09:05 +0530

    serever setup

--- server/src/routes/profileRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { createProfile, updateProfile } = require('../controller/profileController');
-const router = express.Router();
-
-router.post("/create",createProfile);
-router.put("/update/:id",updateProfile);
-
-module.exports = router
