1a9382276dd5f6cac4effb9622a421f4e23ccfa4
nikhil3113 <nikhilgovindchavan@ternaengg.ac.in>
2023-03-18 14:06:20 +0530

    user model

---
+++ server/src/model/user.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const USserSchema = new mongoose.Schema({
+    googleId: {
+        type: String,
+        required: true
+    },
+    displayName: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String
+    },
+    profile: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+
+    }
+},{timestamps: true})
+
+module.exports = mongoose.model('User', UserSchema)
