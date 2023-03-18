8580cefd388f59e083e2347681a9c273338d6e52
Bhushan <bhushanpawar9001@gmail.com>
2023-03-19 03:18:30 +0530

    Authentication done

--- server/index.js
+++ server/index.js
@@ -5,8 +5,10 @@ require('dotenv').config();
 
 const app = express();
 app.use(express.json());
-app.use(cors);
+app.use(cors());
 
 mongoose.connect(process.env.MONGO_URL).then(()=>console.log("Database Connected")).catch((e)=>{e.message})
+const authRoutes = require('./src/routes/userRoutes')
+app.use('/api',authRoutes);
 
 app.listen(process.env.PORT,()=>console.log("server is running on 8080"))
