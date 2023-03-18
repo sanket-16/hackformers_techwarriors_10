113016d74578ea639d0a7874e3ce6f337d0b3c0e
OmkarGurav <omkargurav1920@gmail.com>
2023-03-18 21:46:39 +0530

    setup profile complete

--- client/src/pages/SetUpProfile.tsx
+++ client/src/pages/SetUpProfile.tsx
@@ -35,45 +35,51 @@ const tags: Array<TagProps> = [
         id: '8',
         techstack: 'Javascript',
     },
+
+];
+
+const socials = [
     {
-        id: '9',
-        techstack: 'Javascript',
+        id: '1',
+        title: 'Github',
+        link: 'http://'
     },
     {
-        id: '10',
-        techstack: 'Javascript',
+        id: '2',
+        title: 'snapchat',
+        link: 'http://'
     },
     {
-        id: '11',
-        techstack: 'Javascript',
+        id: '3',
+        title: 'instagram',
+        link: 'http://'
     },
     {
-        id: '12',
-        techstack: 'Javascript',
+        id: '4',
+        title: 'only fans',
+        link: 'http://'
     },
-    {
-        id: '13',
-        techstack: 'Javascript',
-    },
-    {
-        id: '14',
-        techstack: 'Javascript',
-    },
-    {
-        id: '15',
-        techstack: 'Javascript',
-    },
+
+
 ];
 
 const SetUpProfile = () => {
     const [selectedTags, setSelectedTags] = React.useState<Array<TagProps>>([]);
     const [selectedTag, setSelectedTag] = React.useState(tags[0].techstack);
 
-    
+    const [selectedSocials, setSelectedSocials] = React.useState([])
+    const [selectedProfile, setSelectedProfile] = React.useState('')
+
+
+
     return (
 
 
-        <div className='grid grid-cols-1  md:grid-cols-2 w-full '>
+        <form onSubmit={(e)=>{
+            e.preventDefault();
+            console.log(e.target[0].id)
+
+            }} className='grid grid-cols-1  md:grid-cols-2 w-full '>
             <div className=' flex flex-col'>
                 <h3>Setup your Profile.</h3>
                 <div className='flex gap-2  space-x-12 mt-6 '>
@@ -86,23 +92,58 @@ const SetUpProfile = () => {
                         <h2 className='text-2xl '>Sanket Patil</h2>
                     </div>
                 </div>
+
+
+                <div className=''>
+                    <h1>Add Link</h1>
+                    
+                    {selectedSocials.map((social ) => (
+                        <input key={social.id} id={social.title} type="text" />
+                     
+                ))}
+
+                    <select value={selectedProfile} onChange={(e) => {
+                        setSelectedProfile(e.target.value)
+                        if (selectedProfile === e.target.value) {
+                            return;
+                        }
+                        setSelectedSocials([...selectedSocials, socials.find((social) => social.title === e.target.value)])
+
+                    }} className=' h-fit text-sm bg-transparent  text-white font-semibold  py-2 px-2 mx-2 text-center border hover:border-indigo-700  border-blue   rounded-md '>
+                        {socials.map((social) =>
+                            <option key={social.id} onSelect={(e) => console.log(e)} className='bg-almost-black text-white  ' value={social.title}>{social.title}</option>
+                        )}
+
+                    </select>
+
+
+                </div>
+
             </div>
+
+
+
             <div className='grid grid-cols-3 md:grid-cols-5'>
 
                 {selectedTags.map((tag) => (
-                    <Tag key={tag.id} {...tag} />
+                    <Tag  key={tag.id} {...tag} />
                 ))}
-                <select value={selectedTag} onChange={(e)=>{
+                <select value={selectedTag} onChange={(e) => {
                     setSelectedTag(e.target.value)
-                    setSelectedTags([...selectedTags, tags.find((tag)=>tag.techstack===e.target.value)])
-                }} className='text-sm bg-transparent  text-white font-semibold  py-2 px-2 mx-2 text-center border hover:border-indigo-700  border-blue   rounded-md '>
+                    if (selectedTag === e.target.value) {
+                        return;
+                    }
+                    setSelectedTags([...selectedTags, tags.find((tag) => tag.techstack === e.target.value)])
+
+                }} className=' h-fit text-sm bg-transparent  text-white font-semibold  py-2 px-2 mx-2 text-center border hover:border-indigo-700  border-blue   rounded-md '>
                     {tags.map((tag) =>
-                        <option key={tag.id} onSelect={(e)=>console.log(e)} className='bg-almost-black text-white ' value={tag.techstack}>{tag.techstack}</option>
+                        <option key={tag.id} onSelect={(e) => console.log(e)} className='bg-almost-black text-white  ' value={tag.techstack}>{tag.techstack}</option>
                     )}
 
                 </select>
             </div>
-        </div>
+            <button type='submit'  >submit</button>
+        </form>
 
     )
 }
